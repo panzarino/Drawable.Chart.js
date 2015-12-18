c4ebd775638bbecf1609cbd9a457c3adfed60d34
Zach Panzarino <zachary@panzarino.com>
2015-12-18 18:30:35 +0000

    start updating on click

--- src/Drawable.Chart.js
+++ src/Drawable.Chart.js
@@ -12,21 +12,22 @@
 function DrawableChart(id){
     
     // get canvas
-    this.ctx = document.getElementById(id).getContext("2d");
+    this.element = document.getElementById(id);
+    this.ctx = this.element.getContext("2d");
     
     // set data
     var data = {
-        labels: [],
+        labels: [0,10,20],
         datasets: [
             {
                 label: "My First dataset",
-                fillColor: "rgba(220,220,220,0.2)",
-                strokeColor: "rgba(220,220,220,1)",
-                pointColor: "rgba(220,220,220,1)",
+                fillColor: "rgba(220,0,0,.2)",
+                strokeColor: "rgba(220,0,0,1)",
+                pointColor: "rgba(220,0,0,1)",
                 pointStrokeColor: "#fff",
                 pointHighlightFill: "#fff",
                 pointHighlightStroke: "rgba(220,220,220,1)",
-                data: []
+                data: [20,40,10]
             },
         ]
     };
@@ -37,8 +38,18 @@ function DrawableChart(id){
         scaleSteps: 10,
         scaleStepWidth: 10,
         scaleStartValue: 0,
+        showTooltips: true,
     };
     
     // create chart
     this.chart = new Chart(this.ctx).Line(data, options);
+    
+    // updating chart
+    this.update = function(e){
+        
+    };
+    
+    // update when clicked
+    this.element.addEventListener('click',this.update);
+
 }
